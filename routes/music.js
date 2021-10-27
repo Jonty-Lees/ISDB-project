9c8a76587330d4312b8c619c6eae253294d6cb6e
Jonty-Lees <lees.jonty@gmail.com>
2021-10-27 22:35:31 +0100

    renamed trackSchema and added new mongoose Schema

--- routes/music.js
+++ routes/music.js
@@ -1,3 +1,39 @@
-const mongoose = require('mongoose');
+const express = require('express');
+const musicRouter = express.Router();
 
-const 
+
+//  /tracks/:id   GET  all information about specific track, genre and album info
+
+
+
+
+//  /genres   GET   all genres(as an array)
+
+
+
+
+// albums/:id   GET   All info about the specific album, including artist info
+
+
+
+
+// /artists/:id    GET    All info about the specific artist
+
+
+
+
+//  /tracks    POST     you can add Name, album id, Genre id, composer, furation, size in byte and prices.  
+// Json message with created record or proper eroor message. Album and Genre have to exist already
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = musicRouter;
