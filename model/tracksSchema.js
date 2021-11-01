339d0f06f9dde48660e58461e4a93adac64ecd67
Jonty-Lees <lees.jonty@gmail.com>
2021-11-01 12:39:26 +0000

    changed track schema post with required validation

--- model/tracksSchema.js
+++ model/tracksSchema.js
@@ -19,10 +19,22 @@ const trackSchema = new mongoose.Schema({
         min: [1, "Please enter a pre-defined genre between 1-25"],
         max: [25, "Please enter a pre-defined genre between 1-25"]
     },
-    Composer: String,
-    Milliseconds: Number,
-    Bytes: Number,
-    UnitPrice: Number
+    Composer:  {
+        type: String,
+        required: true
+    },
+    Milliseconds: {
+        type: Number,
+        required: true
+    },
+    Bytes: {
+        type: Number,
+        required: true
+    },
+    UnitPrice: {
+        type: Number,
+        required: true
+    }
 })
 
 const Tracks = mongoose.model('Tracks', trackSchema)
